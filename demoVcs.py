a9872c4bf434a7715d418d51432e1435bd80453e
yinxian <ng_yin_xian1@connect.ite.edu.sg>
2021-07-21 11:14:51 +0800

    sum added

---
+++ demoVcs.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def add(num1 ,num2):
+        sum = num1 + num2
+        print("sum is:",sum)
+    def minus(num1 ,num2):
+        sum1 = num1 - num2
+        print("sum is:",sum1)
+Calculator.add(50,40)
+Calculator.minus(50,40)
